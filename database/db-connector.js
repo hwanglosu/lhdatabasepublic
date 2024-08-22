8043333b41c383db83c37cf4977cdc9eaa619813
Louis A Hwang <114049284+hwanglosu@users.noreply.github.com>
2024-08-22 12:04:14 -0400

    Update db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -11,10 +11,10 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_',
+    host            : '',
+    user            : '',
     password        : '',
-    database        : 'cs340_'
+    database        : ''
 })
 
 // Export it for use in our applicaiton
